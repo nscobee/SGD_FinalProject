0eadede337f56ffeb21617bf40743f0c77876e07
Nicholas Scobee <nscobee@stumail.jccc.edu>
2019-05-23 11:59:27 -0500

    Added main Menu

---
+++ finalProject/Assets/scripts/MainMenuLogic.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuLogic : MonoBehaviour
+{
+
+    public GameObject MainMenuPanel;
+    public GameObject CreditsPanel;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainMenuPanel.SetActive(true);
+        CreditsPanel.SetActive(false);
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Exercise 1");
+    }
+
+    public void Credits()
+    {
+        MainMenuPanel.SetActive(false);
+        CreditsPanel.SetActive(true);
+    }
+
+    public void MainMenu()
+    {
+        MainMenuPanel.SetActive(true);
+        CreditsPanel.SetActive(false);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
